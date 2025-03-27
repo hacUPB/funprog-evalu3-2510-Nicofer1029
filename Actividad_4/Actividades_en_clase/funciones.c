8057481edc77e1266c30dbc81c8ce6cb91cd0a32
Nicolás Fernández Barreto <nicolas.fernandezb@upb.edu.co>
2025-03-27 10:40:39 -0500

    Actividad 4 funciones y Retos

---
+++ Actividad_4/Actividades_en_clase/funciones.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void imprimir (void);
+
+int main(){
+    
+    imprimir ();
+    
+    return 0;
+}
+
+void imprimir(void){
+
+
+    printf("************************************************** \n");
+    printf("* \t Universidad Pontificia Bolivariana \t * \n");
+    printf("* \t Escuela de Ingenierias \t \t * \n");
+    printf("* \t Ingenieria Electrica \t \t \t * \n");
+    printf("* \t Nicolas Fernandez Barreto \t \t * \n");
+    printf("* \t ID: 00057904 \t \t \t \t * \n");
+    printf("**************************************************");
+
+}
---
+++ Retos/Factor_potencia.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+
+int menu(void);
+
+int main(int argc, char const *argv[])
+{
+    float amperios, resistencia, voltaje;
+
+    int opcion = menu();
+    
+    switch (opcion)
+    {
+
+    case 1:
+        printf("Ingresa el valor de la potencia aparente \n");
+        scanf("%f", &amperios);
+        printf("Ingresa el valor de la resistencia (R) en ohmios \n");
+        scanf("%f", &resistencia);
+        voltaje = amperios*resistencia;
+        printf("La tension (V) es: %.2f" , voltaje);
+        printf(" Voltios");
+    break;
+
+    case 2:
+        printf("Ingresa el valor del voltaje (V) en voltios \n");
+        scanf("%f", &voltaje);
+        printf("Ingresa el valor de la resistencia (R) en ohmios \n");
+        scanf("%f", &resistencia);
+        amperios = voltaje/resistencia;
+        printf("La corriente (I) es: %.2f" , amperios);
+        printf(" Amperios");
+    break;
+
+    case 3:
+        printf("Ingresa el valor del voltaje (V) en voltios \n");
+        scanf("%f", &voltaje);
+        printf("Ingresa el valor de la corriente (I) en amperios \n");
+        scanf("%f", &amperios);
+        resistencia = voltaje/amperios;
+        printf("La corriente (I) es: %.2f" , resistencia);
+        printf(" Ohmios");
+    break;
+    
+    case 4:
+    printf("Volver al menu principal \n");
+    break;
+
+    default:
+        printf("La opcion que usted eligio no es valida");
+    break;
+    }
+
+    return 0;
+}
+
+int menu(void){
+
+    int opcion;
+
+    printf("Has seleccionado calcular el Factor de Potencia. \n");
+    printf("¿Que parametro deseas calcular? \n");
+    printf("1. Calcular Potencia Activa. \n 2. Calcular Potencia Aparente. \n 3. Calcular Factor de Potencia. \n");
+    printf("Ingresa el numero de la opcion deseada \n");
+    scanf("%d", &opcion);
+
+    return opcion;
+
+}
